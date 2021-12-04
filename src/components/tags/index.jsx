eddccc7e201475e3d641663e3f37afa6379b05cb
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-04 15:11:53 +0400

    added style for tags

--- src/components/tags/index.jsx
+++ src/components/tags/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import {Body3} from 'typography'
+import {Container, Tags, Name } from './styled'
 
-export default function İndex() {
+export default function Index() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Tags>
+                <Name>
+                    <Body3>All</Body3>
+                </Name>
+                <Name>
+                    <Body3>UI</Body3>
+                </Name>
+                <Name>
+                    <Body3>UX</Body3>
+                </Name>
+                <Name>
+                    <Body3>Enhancement</Body3>
+                </Name>
+                <Name>
+                    <Body3>Bug</Body3>
+                </Name>
+                <Name>
+                    <Body3>Feature</Body3>
+                </Name>
+            </Tags>
+        </Container>
     )
 }
---
+++ src/components/tags/styled/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import colors from 'color'
+
+export const Container = styled.div`
+    background: ${colors.white};
+    height: 137px;
+    width: 255px; 
+    border-radius: 10px; 
+    padding: 24px;
+    display: flex; 
+    flex-wrap: wrap;
+`
+
+export const Tags = styled.div`
+    padding: 5px 16px;
+    background-color: ${colors.ultramarineBlue};
+`
+
+export const Name = styled.div`
+
+`
