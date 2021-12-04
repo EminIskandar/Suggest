a53a112668fbc68630ab30d770d4b7325f370707
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-04 14:29:27 +0400

    added html and css for board

---
+++ src/components/board/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { H2, Body2 } from '../../theme/typography'
+import {Container , Text } from './styled'
+
+export default function index() {
+    return (
+         <Container>
+             <Text>
+                <H2>Frontend Mentor</H2>
+                <Body2>Feedback Board</Body2>
+             </Text>
+         </Container>
+    )
+}
---
+++ src/components/board/styled/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background: radial-gradient(128.88% 128.88% at 103.9% -10.39%, #E84D70 0%, #A337F6 53.09%, #28A7ED 100%);
+    height: 137px;
+    width: 255px; 
+    border-radius: 10px; 
+    padding: 24px;
+    display: flex;
+    justify-content: flex-end;
+`
+export const Text = styled.div`
+
+`
