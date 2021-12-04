e23ffd184bb699fb8b3190a9bad37d8fa697d440
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-04 17:32:36 +0400

    added media query for main theme component

--- src/theme/components.js
+++ src/theme/components.js
@@ -5,6 +5,16 @@ export const Main = styled.main`
     width: 1440px; 
     margin: 0 auto;
     padding: 94px 165px 0; 
+
+    @media (max-width:1440px){  
+        width: 1110px;
+        padding: 94px 0px 0; 
+    }
+
+    @media (max-width:1200px){  
+        width: 100vw;
+        padding: 94px 20px 0; 
+    }
 `
 export const button = styled.button`
     height: 44px;
