c156db81be4bc1cb3c8ae152a321c2aa8275064e
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-04 14:21:44 +0400

    created board components

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -2,11 +2,9 @@ import styled from "styled-components";
 
 export const H1 = styled.h1` 
     font-size: 24px;
-    
     font-weight: 700;
     line-height: 35px;
     letter-spacing: -0.3333333432674408px;
-     
 `
 
 export const H2 = styled.h2` 
