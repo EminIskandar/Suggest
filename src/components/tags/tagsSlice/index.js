a03a0cf6d3ed25e7725aa498a3d72b8c59d740e0
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 10:26:39 +0400

    added redux for tags components

---
+++ src/components/tags/tagsSlice/index.js
@@ -0,0 +1,13 @@
+import {  createSlice } from "@reduxjs/toolkit"; 
+  
+const  initialState = 'all'
+
+const tagsSlice = createSlice({
+    name: "tags",
+    initialState: initialState,
+    reducers:  {}
+});
+    
+export default tagsSlice;
+     
+   
