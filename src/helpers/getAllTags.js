be5e3f5fcf7a6e300f14a08a0b3036ef7a7ac864
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 09:18:12 +0400

    added code to func getAllTags

--- src/helpers/getAllTags.js
+++ src/helpers/getAllTags.js
@@ -1,3 +1,16 @@
-export default function getAllTags (){
+import db from '../db/db.json'
 
+export default function getAllTags (){
+    const allSuggest = db.productRequests
+    const allTags = ['all']
+
+    allSuggest.forEach( ({category}) => {
+        const ifHaveValue = allTags.some( name => name === category)
+
+        if(!ifHaveValue){
+            allTags.push(category)
+        } 
+    })
+
+    return allTags;
 }
