7b16ae6469d2eff523fccaa5b106d6b0c637e450
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 10:30:30 +0400

    combine reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,10 @@
-const reducer = {}
+import tagsSlice from 'components/tags/tagsSlice'
+import suggestionsSlice from 'components/suggestions/suggestionsSlice'
+
+const reducer = {
+    selectedTag :  tagsSlice,
+    suggests : suggestionsSlice,
+}
+
 
 export default reducer
