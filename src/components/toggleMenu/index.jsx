4ecb14c128b646e1db036b07bc73eab228f77fbe
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 11:57:43 +0400

    added useSelector and some code to toggleMenu component

--- src/components/toggleMenu/index.jsx
+++ src/components/toggleMenu/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Tags, Roadmap} from 'components'
 import {Container, Content} from './styled'
+import { useSelector } from 'react-redux'
 
 export default function Index() {
+    const isOpen = useSelector(state => state.isToggleMenuOpen ) 
     return (
         <Container>
-            <Content>
+            <Content className={isOpen.payload ? 'open' : ''}>
                 <Tags theme="toggle-menu"/>
                 <Roadmap theme="toggle-menu"/>
             </Content>
