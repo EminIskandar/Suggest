2370de45269b9ee1af8e6643f4ba33d59becafb0
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 12:21:24 +0400

    added dinamic status for roadmap component

--- src/components/roadmap/index.jsx
+++ src/components/roadmap/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react' 
 import {Container, Header, Title, Link, Lists, ListsItem, Name, Count} from './styled'
+import getSuggetsStatus from 'helpers/getSuggetsStatus'
 
 export default function Index({theme}) {
+
+    const status = getSuggetsStatus()
+
     return (
         <Container theme={theme}>
             <Header>
@@ -11,15 +15,15 @@ export default function Index({theme}) {
             <Lists>
                 <ListsItem>
                     <Name>Planned</Name>
-                    <Count>2</Count>
+                    <Count>{status.planned}</Count>
                 </ListsItem>
                 <ListsItem>
                     <Name>In-Progress</Name>
-                    <Count>3</Count>
+                    <Count>{status.inProgress}</Count>
                 </ListsItem>
                 <ListsItem>
                     <Name>Live</Name>
-                    <Count>1</Count>
+                    <Count>{status.live}</Count>
                 </ListsItem>
             </Lists>
         </Container>
