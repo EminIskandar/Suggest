a60e823fa3cfb844b6cc0f4008305e29ff767bfd
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 11:50:57 +0400

    added func handleToggleMenu and dispatch

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react' 
 import {Container , Text, Title, Category, ToggleMenu } from './styled' 
+import { change } from './boardSlice'
+import { useDispatch } from 'react-redux'
 
 export default function Index() {
+
+    const dispatch = useDispatch()
+
+    const handleToggleMenu = () => { 
+    }
+
     return (
          <Container>
              <Text>
                  <Title>Frontend Mentor</Title>
                  <Category>Feedback Board</Category> 
              </Text>
-             <ToggleMenu className=''>
+             <ToggleMenu className='' onClick={handleToggleMenu}>
                  <ul>
                      <li></li>
                      <li></li>
