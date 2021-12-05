baa1791d45bd530b5ec1dced2ca7c570c94503e0
emin.iskandar <emin.iskandarzada@gmail.com>
2021-12-05 11:52:43 +0400

    boardSlice is add to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
 import tagsSlice from 'components/tags/tagsSlice'
 import suggestionsSlice from 'components/suggestions/suggestionsSlice'
+import boardSlice from '../components/board/boardSlice'
 
 const reducer = {
     selectedTag :  tagsSlice.reducer,
     suggests : suggestionsSlice.reducer,
+    isToggleMenuOpen : boardSlice.reducer
 }
 
 
